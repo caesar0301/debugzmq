dd6f7588746a6d28cf7ab2d41e561ce6d6aba0aa
xiaming.cxm <xiaming.cxm@alibaba-inc.com>
2025-05-16 16:53:21 +0800

    Update: "build.sh"

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-gcc server.c -o server
-gcc client.c -o client
+gcc -std=c99 server.c -o server
+gcc -std=c99 client.c -o client
 
-gcc zserver.c -o zserver -lzmq
-gcc zclient.c -o zclient -lzmq
+gcc -std=c99 zserver.c -o zserver -lzmq
+gcc -std=c99 zclient.c -o zclient -lzmq
